8c1b7163757b4d8c80f41b0d6f2f8cf33bfd9c36
tim <nagisa12164@163.com>
2024-11-26 19:29:18 +0800

    feat: add pages

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ for website in news_websites:
         crawl_status = app.crawl_url(
             website,
             params={
-                'limit': 1,
+                'limit': 10,
                 'scrapeOptions': {'formats': ['markdown', 'html']}
             },
             poll_interval=1
